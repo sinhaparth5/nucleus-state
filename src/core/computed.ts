af6e4f3d9f6c8ea7cdccb055e1cc64d5b58cb584
Parth Sinha <sinhaparth555@gmail.com>
2025-06-27 20:42:32 +0100

    updated computed file in core

--- src/core/computed.ts
+++ src/core/computed.ts
@@ -1,27 +1,33 @@
 import type { Atom } from '../types';
 
-export function createComputed<T>(
-  computeFn: () => T
-): Atom<T> {
+export function createComputed<T>(computeFn: () => T): Atom<T> {
   let value = computeFn();
+  let isStale = false;
   const listeners = new Set<(value: T) => void>();
-  
-  // TODO: Add dependency tracking in future versions
-  
-  return {
+
+  const atom: Atom<T> = {
     get(): T {
+      if (isStale) {
+        const newValue = computeFn();
+        if (!Object.is(newValue, value)) {
+          value = newValue;
+          listeners.forEach(listener => listener(value));
+        }
+        isStale = false;
+      }
       return value;
     },
-    
+
     set(): void {
       throw new Error('Cannot set value on computed atom');
     },
-    
-    subscribe(listener: (value: T) => void): () => void {
+
+    subscribe(listener: (value: T) => void) {
       listeners.add(listener);
       return () => {
         listeners.delete(listener);
       };
-    }
+    },
   };
+  return atom;
 }
