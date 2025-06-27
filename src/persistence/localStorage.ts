1e41484d946de5f3bf152de5196bdcee71c0dee9
Parth Sinha <sinhaparth555@gmail.com>
2025-06-27 20:50:45 +0100

    updated localstorage

---
+++ src/persistence/localStorage.ts
@@ -0,0 +1,26 @@
+import { createAtom } from "../core";
+import type { AtomOptions } from "../types";
+
+export function createPresistedAtom<T>(
+    initialValue: T,
+    key: string,
+    options: Omit<AtomOptions, 'persist'> = {}
+) {
+    return createAtom(initialValue, {
+        ...options,
+        persist: key,
+        storage: localStorage,
+    });
+}
+
+export function createSessionAtom<T>(
+    initialValue: T,
+    key: string,
+    options: Omit<AtomOptions, 'persist'> = {}
+) {
+    return createAtom(initialValue, {
+        ...options,
+        persist: key,
+        storage: sessionStorage,
+    });
+}
