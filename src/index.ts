18020e575421cc0065e5315c08d636e16d4057eb
Parth Sinha <sinhaparth555@gmail.com>
2025-06-27 20:56:07 +0100

    updated main src index file

--- src/index.ts
+++ src/index.ts
@@ -4,5 +4,15 @@ export { createAtom, createComputed } from './core';
 // React exports
 export { useAtom, useAtomValue, useSetAtom } from './react';
 
+// Persistence exports
+export { createPersistedAtom, createSessionAtom } from './persistence';
+
 // Types
-export type { Atom, AtomOptions } from './types';
+export type {
+    Atom, 
+    AtomOptions,
+    AtomListener,
+    AtomGetter,
+    AtomSetter,
+    Unsubscribe
+} from './types';
