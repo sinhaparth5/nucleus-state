a27902cd5e949d6688dd73ee361b031c4061e8cd
Parth Sinha <parth.sinha@webcurl.co.uk>
2025-06-27 16:44:00 +0100

    updated index file

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+export { createAtom } from './core';
+export { createComputed } from './core';
+
+export { useAtom, useAtomValue, useSetAtom } from './react';
+
+export type { Atom, AtomOptions } from './types';
