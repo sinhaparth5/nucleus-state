c5495bc7937b3676d3f846f6538752c38ae2c8be
Parth Sinha <parth.sinha@webcurl.co.uk>
2025-06-27 16:47:36 +0100

    fixed typos

--- tsup.config.ts
+++ tsup.config.ts
@@ -4,7 +4,7 @@ export default defineConfig({
   entry: ['src/index.ts'],
   format: ['cjs', 'esm'],
   dts: true,
-  spliting: false,
+  splitting: false,
   sourcemap: true,
   clean: true,
   minify: true,
